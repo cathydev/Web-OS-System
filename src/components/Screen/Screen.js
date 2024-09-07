d2fd5c12cd0519921c747060f53fef973bdad470
Catherine <catherinemdsilva@gmail.com>
2024-09-06 20:12:21 -0400

    Creating taskbar

--- src/components/Screen/Screen.js
+++ src/components/Screen/Screen.js
@@ -28,78 +28,94 @@ const Computer = () => {
         link.click();
         document.body.removeChild(link);
     };
+    const items = [
+        {
+            title: 'About Me',
+            src: portrait,
+            alt: 'My custom SVG',
+            onClick: () => toggleComponent('About Me'),
+        },
+        {
+            title: 'Projects',
+            src: projects,
+            alt: 'projects illustration',
+            onClick: () => toggleComponent('Projects'),
+        },
+        {
+            title: 'Contact Me',
+            src: email,
+            alt: 'email illustration',
+            onClick: () => toggleComponent('Contact Me'),
+        },
+        {
+            title: 'Download CV',
+            src: download,
+            alt: 'download icon',
+            onClick: onButtonClick,
+        },
+        {
+            title: 'Thank You',
+            src: heart,
+            alt: 'a heart',
+            onClick: () => toggleComponent('Thank You'),
+        },
+    ];
 
     return (
         <div className="container">
-            <div className="base"> 
+            <div className="base">
                 <div className={styles.screen}>
-                    {activeComponent === 'projects' && <Projects close={() => toggleComponent('projects')} />}
-                    {activeComponent === 'about' && <AboutMe close={() => toggleComponent('about')} />}
-                    {activeComponent === 'contact' && <ContactMe close={() => toggleComponent('contact')} />}
-                    {activeComponent === 'thanks' && <ThankYou close={() => toggleComponent('thanks')} />}
+                    {activeComponent === 'Projects' && <Projects close={() => toggleComponent('Projects')} />}
+                    {activeComponent === 'About Me' && <AboutMe close={() => toggleComponent('About Me')} />}
+                    {activeComponent === 'Contact Me' && <ContactMe close={() => toggleComponent('Contact Me')} />}
+                    {activeComponent === 'Thank You' && <ThankYou close={() => toggleComponent('Thank You')} />}
                     <div>
                         <ul className={styles.apps}>
-                            <li>
-                                <Tooltip title="About Me">
-                                    <Image
-                                        src={portrait}
-                                        alt="My custom SVG"
-                                        width={60}
-                                        height={60}
-                                        onClick={() => toggleComponent('about')}
-                                    />
-                                </Tooltip>
-                                <div>About me</div>
-                            </li>
-                            <li>
-                                <Tooltip title="Projects">
-                                    <Image
-                                        src={projects}
-                                        alt="projects illustration"
-                                        width={60}
-                                        height={60}
-                                        onClick={() => toggleComponent('projects')}
-                                    />
-                                </Tooltip>
-                                <div>Projects</div>
-                            </li>
-                            <li>
-                                <Tooltip title="Contact Me">
-                                <Image
-                                        src={email}
-                                        alt="email illustration"
-                                        width={60}
-                                        height={60}
-                                        onClick={() => toggleComponent('contact')}
-                                    />
-                                </Tooltip>
-                                <div>Contact me</div>
-                            </li>
-                            <li>
-                                <Tooltip title="Download CV">
-                                    <Image
-                                        src={download}
-                                        alt="download icon"
-                                        width={60}
-                                        height={60}
-                                        onClick={onButtonClick}
-                                    />
-                                </Tooltip>
-                                <div>CV</div>
-                            </li>
-                            <li>
-                                <Tooltip title="Thank you">
-                                    <Image
-                                        src={heart}
-                                        alt="a heart"
-                                        width={60}
-                                        height={60}
-                                        onClick={() => toggleComponent('thanks')}
-                                    />
-                                </Tooltip>
-                                <div>Thank you</div>
-                            </li>
+                            {items.map((item, index) => (
+                                <li key={index}>
+                                    <Tooltip title={item.title}>
+                                        <Image
+                                            src={item.src}
+                                            alt={item.alt}
+                                            width={60}
+                                            height={70}
+                                            onClick={item.onClick}
+                                        />
+                                    </Tooltip>
+                                    <div>{item.title}</div>
+                                </li>
+                            ))}
                         </ul>
+                    </div>
+                    <div className={styles.taskbar}>
+                        <div className={styles.taskbarContent}>
+                            <Image
+                                src={"/Icons/menu.png"}
+                                alt="menu icon"
+                                width={35}
+                                height={35}
+                            />
+                            {activeComponent &&
+                                <>
+                                    {items.map((item, index) => (
+                                        item.title === activeComponent && (
+                                            <div key={index} className={styles.open_window}>
+                                                <Tooltip title={item.title}>
+                                                    <Image
+                                                        src={item.src}
+                                                        alt={item.alt}
+                                                        width={25}
+                                                        height={25}
+                                                        onClick={item.onClick}
+                                                    />
+                                                </Tooltip>
+                                               <span>{item.title}</span> 
+                                            </div>
+                                        )
+                                    ))}
+                                </>
+                            }
+                        </div>
                     </div>
                 </div>
             </div>
