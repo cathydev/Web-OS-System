27a61ac17565d3046552abd492c937d27008dee1
Catherine <catherinemdsilva@gmail.com>
2024-09-30 22:12:20 -0400

    Add useMemo to currentTime

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,23 +1,27 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import styles from "../../styles/Login.module.css";
 
 const Login = ({ mount }) => {
   const [currentTime, setCurrentTime] = useState(new Date());
 
-  useEffect(() => {
-    const timer = setInterval(() => {
+  const setCurrentTimeMemo = useMemo(() => {
+    return () => {
       setCurrentTime(new Date());
-    }, 1000);
+    };
+  }, [setCurrentTime]);
+
+  useEffect(() => {
+    const timer = setInterval(setCurrentTimeMemo, 1000);
 
     return () => clearInterval(timer);
-  }, []);
+  }, [setCurrentTimeMemo]);
 
   return (
     <div className="container">
       <div className="base">
         <div className={styles.screen}>
           <div className={styles.start}>
-            <span className={styles.time}>
+            <span className={styles.time} suppressHydrationWarning>
               {currentTime.toLocaleTimeString([], {
                 hour: "2-digit",
                 minute: "2-digit",
